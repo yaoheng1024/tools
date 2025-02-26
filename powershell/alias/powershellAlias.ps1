66d59cf4ee2d9755bc273966b4dabd119ae74c1c
潘耀恒 <panyaoheng@wps.cn>
2025-02-26 15:01:37 +0800

    feat: 添加powerShell工具集

---
+++ powershell/alias/powershellAlias.ps1
@@ -0,0 +1,130 @@
+function showLog {
+  param([string] $msg)
+  Write-Host $msg -ForegroundColor Green
+}
+
+# 提交bug
+function _cm {
+  param([string] $commitMsg, [bool] $skipVerify)
+  showLog "git commit -m `"$commitMsg`""
+  if ($skipVerify) {
+    git commit -m $commitMsg --no-verify
+    return;
+  }
+  git commit -m $commitMsg
+}
+
+function cmb {
+  param([string] $id, [string] $msg, [bool] $skipVerify)
+  $commitMsg = "fix(otl$id): $msg"
+  _cm $commitMsg $skipVerify
+}
+function cmbAndPush {
+  param([string] $id, [string] $msg, [bool] $skipVerify)
+  cmb $id $msg $skipVerify
+  $gitStatus = git status
+  if (!($gitStatus -match 'Changes to be committed') -and ($gitStatus -match 'Your branch and .* have diverged' -or $gitStatus -match 'Your branch is ahead of .* by')) {
+    return push;
+  }
+  showLog("commit失败了")
+}
+# 提交feature
+function cmf {
+  param([string] $msg, [bool] $skipVerify)
+  $commitMsg = "feat(otl): $msg"
+  _cm $commitMsg $skipVerify
+}
+
+function parseGitStatusResult {
+  param ([string] $msg)
+  if ($msg -match 'Your branch is behind') {
+
+  }
+}
+
+function pull {
+  showLog "开始从远端获取最新的代码信息"
+  git fetch
+  $gitStatus = git status
+  if ($gitStatus -match 'Your branch is behind' -or $gitStatus -match 'Your branch and .* have diverged') {
+    showLog "当前分支与远端分支有提交交叉，准备使用rebase更新代码"
+    showLog "执行stash操作"
+    git stash -u
+    showLog "开始从远端拉取最新的代码"
+    git pull --rebase
+    showLog "执行stash pop操作"
+    git stash pop
+  }
+  else {
+    git pull
+  }
+}
+
+function push {
+  pull
+  showLog "开始推送代码到远端"
+  git push
+}
+
+function resetToPrevCommit {
+  param([bool] $isHard)
+  if ($isHard) {
+    git reset --hard HEAD^
+  }
+  else {
+    git reset --soft HEAD^
+  }
+}
+
+function resetToBranch {
+  param([string] $branch)
+  git fetch
+  git reset --hard origin/$branch
+}
+
+function merge {
+  param([string] $branch)
+  git fetch
+  git pull origin $branch
+}
+
+function deleteBranch {
+  param([string] $branch)
+  git branch -d $branch
+  git push origin --delete $branch
+}
+
+function buildOtl {
+  showLog "开始打包wpsweb..."
+  cd D:\workspace\wpsweb\client\app
+  node --stack-size=8092 --max-old-space-size=8092 ./node_modules/gulp/bin/gulp.js --sourcemap --outline
+}
+
+function startWst {
+  showLog "启动whistle..."
+  w2 start -n pyh_whistle -w pyh159357
+}
+
+function startClipboardMonitor {
+  showLog "启动剪贴板监听..."
+  $job = Get-Job -Name clipboardMonitor -ErrorAction Ignore
+  if ($job) {
+    if ("Running" -eq $job.State) {
+      return
+    }
+    $job | Remove-Job
+  }
+  try {
+    Start-Job -Name clipboardMonitor -FilePath $PSHOME\customScript\clipboardMonitor.ps1
+  }
+  catch {
+    showLog "An error occurred:"
+    showLog $_
+  }
+}
+
+function startWps {
+  startClipboardMonitor
+  startWst
+  buildOtl
+}
---
+++ powershell/clipboardMonitor.ps1
@@ -0,0 +1,34 @@
+Write-Verbose -Message "开始监控剪贴板" -Verbose
+# 创建一个无限循环，监视剪贴板的变化
+while ($true) {
+    # 获取当前剪贴板的内容
+    $clipboardData = Get-Clipboard -Raw
+    $newClipboardData = ""
+    Write-Verbose -Message $clipboardData -Verbose
+    
+    # 从vscode到浏览器
+    if ($clipboardData -match '^client.app.applications') {
+        # 浏览器里路径是以/分隔的，Windows vscode复制出来的是\，要替换掉
+        $newClipboardData = $clipboardData -replace '\\', "/"
+        # 打包好后的文件路径不带client/app的，所以要去掉
+        $newClipboardData = $newClipboardData -replace 'client/app', ""
+    } elseif ($clipboardData -match '^webpack(?:-internal)?:///applications') {
+        # 从浏览器到vscode
+        $newClipboardData = $clipboardData -replace 'webpack(?:-internal)?:///applications/', ""
+        $newClipboardData = $newClipboardData -replace '\?.*$', ""
+    } elseif ($clipboardData -match '^【金山文档.+\n(https.*)') {
+        # 改写金山文档分享链接格式
+        $newClipboardData = $Matches.1
+    }
+    
+    if ($newClipboardData) {
+        #替换完成，写入剪贴板
+        Set-Clipboard $newClipboardData
+    
+        # Clear-Host
+        $output = "改写成功: " + $newClipboardData
+        Write-Verbose -Message $output -Verbose
+        # 等待一段时间再次检查剪贴板
+    }
+    Start-Sleep -Seconds 1
+}
